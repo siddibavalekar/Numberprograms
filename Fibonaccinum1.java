3fb70837214973698f4f1f871248c82223deab14
Siddhi <131698330+siddibavalekar@users.noreply.github.com>
2024-01-15 23:20:05 +0530

    Fibonacci_Series

---
+++ Fibonaccinum1.java
@@ -0,0 +1,19 @@
+class Fibonaccinum1
+{
+public static void main(String args[])
+{
+int a=0;
+System.out.println(a);
+int b=1; 
+System.out.println(b);
+for(int i=1;i<=10;i++)
+{
+int c=a+b;
+a=b;
+b=c;
+System.out.println(c);
+
+}
+}
+}
+
